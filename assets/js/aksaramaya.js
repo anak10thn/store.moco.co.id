9d049612da78cacbe1cb6768628433f314c01aa7
anak10thn <linux@toroo.org>
2014-01-22 00:05:31 +0700

    ADD: fix file

--- assets/js/aksaramaya.js
+++ assets/js/aksaramaya.js
@@ -1,6 +1,20 @@
 var base_url = 'http://store.aksaramaya.com/apis';
 var client = 'NTEwMzg4M2IxYjdjM2M3';
 
+main = (function(){
+    return{
+        lenghtObject : function(obj){
+          var result = 0;
+          for(var prop in obj) {
+            if (obj.hasOwnProperty(prop)) {
+              result++;
+            }
+          }
+          return result;
+        }
+    }
+})();
+
 book = (function(){
     return{
         slider : function(value){
