cb97fe4f3dc03b8d6babd9ff813d80320f8728cc
anak10thn <linux@toroo.org>
2014-01-22 04:51:08 +0700

    FIX ROUTE

--- routes/routes.js
+++ routes/routes.js
@@ -1,8 +1,15 @@
 module.exports = function(app,port,superagent,async,base_url,client,fs,secret){
 
     app.get('/',function(req, res){
-        console.log(req.session.name);
-        return res.render('index',{"title":"Moco Store"});
+        if(!req.session.login){
+            var login = "";
+            var user = "";
+        }
+        else{
+            var login = req.session.login;
+            var user = req.session.user;
+        }
+        return res.render('index',{"title":"Moco Store","login":login,"user":user});
     });
     
     app.get('/category',function(req, res){
@@ -20,7 +27,14 @@ module.exports = function(app,port,superagent,async,base_url,client,fs,secret){
     });
     
     app.get('/user',function(req, res){
-        return res.render('user',{"title":"Moco Store"}); 
+        if(!req.session.login){
+            return res.redirect('/');
+        }
+        else{
+            var login = req.session.login;
+            var user = req.session.user;
+        }
+        return res.render('user',{"title":"Moco Store","login":login,"user":user}); 
     });
     
     app.post('/signup',function(req, res){
@@ -30,14 +44,41 @@ module.exports = function(app,port,superagent,async,base_url,client,fs,secret){
                device_id : 1,
                name : req.body.name,
                username : req.body.email,
-               password : req.body.password
+               password : req.body.password,
+               confirm_password : req.body.repassword
               })
         .end(function(err,data){
             return res.send(data.text);
         });
     });
+    
+    app.post('/login',function(req, res){
+        superagent.post(base_url+"/users/login")
+        .send({client_id : client,
+               client_secret : secret,
+               device_id : 1,
+               username : req.body.username,
+               password : req.body.password
+              })
+        .end(function(err,data){
+            var code = JSON.parse(data.text);
+            if(code.meta.code == 200){
+                req.session.login = code.data;
+                superagent.get(base_url+"/users/profile?access_token="+code.data.access_token)
+                .end(function(err,data){
+                    req.session.user = JSON.parse(data.text).data.User;
+                    return res.redirect('/user');
+                });
+            }
+            else{
+                return res.redirect('/');
+            }
+        });
+    });
+    
     app.get('/ses',function(req, res){
-        req.session.name = "ibnu";
-        return res.send(req.session.name);
+        console.log(JSON.stringify(req.session.login));
+        console.log(JSON.stringify(req.session.user));
+        return false;
     });
 };
